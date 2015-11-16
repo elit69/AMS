ea032641b9fa8628f8a5e732c8ef8092f2447e77
Chhorn Elit <chhornelit@gmail.com>
2015-11-16 16:17:23 +0700

    update user dto

--- src/main/java/com/ams/entities/User.java
+++ src/main/java/com/ams/entities/User.java
@@ -1,92 +1,74 @@
 package com.ams.entities;
 
-import java.sql.Date;
-
 public class User {
 	private int id;
 	private String username;
-	private String email;
 	private String password;
-	private Date birthdate;
-	private Date registerdate;
-	private String image;
-	private String role;
 	private boolean enable;
-	
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-	public boolean isEnable() {
-		return enable;
-	}
-
-	public void setEnable(boolean enable) {
-		this.enable = enable;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
+	private String email;	
+	private String address;
+	private String phone;
+	private String name;
+	private char gender;
+	private String image;
 	public int getId() {
 		return id;
 	}
-
-	public int getUserId(){
-		return id;
-	}
-	
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isEnable() {
+		return enable;
+	}
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	public Date getBirthdate() {
-		return birthdate;
+	public String getAddress() {
+		return address;
 	}
-
-	public void setBirthdate(Date birthdate) {
-		this.birthdate = birthdate;
+	public void setAddress(String address) {
+		this.address = address;
 	}
-	
-	public Date getRegisterdate() {
-		return registerdate;
+	public String getPhone() {
+		return phone;
 	}
-
-	public void setRegisterdate(Date registerdate) {
-		this.registerdate = registerdate;
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
-
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
 	public String getImage() {
 		return image;
 	}
-
 	public void setImage(String image) {
 		this.image = image;
 	}
-	
 }
