a33b0b1e5684525237b955b1c05664771fba3175
phalleak <lachphalleak70@gmail.com>
2015-11-16 16:44:30 +0700

    articledto

---
+++ src/main/java/com/ams/entities/Article.java
@@ -0,0 +1,61 @@
+package com.ams.entities;
+
+public class Article {
+	
+	private int id;
+	private String title;
+	private int userid;
+	private String content;
+	private String pubdate;
+	private int enable;
+	private String image;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPubdate() {
+		return pubdate;
+	}
+	public void setPubdate(String pubdaye) {
+		this.pubdate = pubdaye;
+	}
+	public int getEnable() {
+		return enable;
+	}
+	public void setEnable(int enable) {
+		this.enable = enable;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", title=" + title + ", userid=" + userid
+				+ ", content=" + content + ", pubdaye=" + pubdate + ", enable="
+				+ enable + ", image=" + image + "]";
+	}
+	
+}
