e314066692f92d0819ce0becbbc2cdf2965bc878
Chhorn Elit <chhornelit@gmail.com>
2015-12-24 15:09:59 +0700

    Revert "sdf"
    This reverts commit e55a98cd50dc46134cc9d8eab33342938c2b29be.

--- src/main/java/com/ams/app/services/UserRoleService.java
+++ src/main/java/com/ams/app/services/UserRoleService.java
@@ -3,7 +3,7 @@ package com.ams.app.services;
 import java.util.List;
 
 public interface UserRoleService {
-	public boolean insert(int userid, List<String> role);
+	public boolean insert(int userid, String role);
 	//public boolean updateUserRole(int roleid, int userid);
 	public List<String> list(int usrId);
 }
